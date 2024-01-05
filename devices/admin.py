bbe36100aa15f5362805a868e508f92a6c71bbcd
bekzod818 <bekzod030900@gmail.com>
2024-01-06 00:22:18 +0500

    fix: setup_project_and_app

---
+++ devices/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from .models import Location, Device
+
+
+admin.site.register(Location)
+admin.site.register(Device)
---
+++ devices/models.py
@@ -0,0 +1,20 @@
+import uuid
+from django.db import models
+from django_extensions.db.fields import AutoSlugField
+
+
+class Location(models.Model):
+    name = models.CharField(max_length=200)
+
+    def __str__(self) -> str:
+        return self.name
+    
+
+class Device(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    name = models.CharField(max_length=200)
+    slug = AutoSlugField(populate_from='name')
+    location = models.ForeignKey(Location, on_delete=models.SET_NULL, null=True, blank=True)
+
+    def __str__(self) -> str:
+        return f"{self.name} - {self.id}"
