c65f57d6964ebd244f98289deb1089648a3a3375
bekzod818 <bekzod030900@gmail.com>
2024-01-08 00:32:56 +0500

    fix: delete_device

--- devices/api.py
+++ devices/api.py
@@ -41,7 +41,9 @@ def get_device_detail(request, slug: str):
 @app.put("/devices/{slug}", response=DeviceSchema)
 def update_device(request, slug: str, device: DeviceUpdateSchema):
     device_model = get_object_or_404(Device, slug=slug)
-    for attr, value in device.dict().items():
+    for attr, value in device.dict(
+        exclude_unset=True
+    ).items():  # Partial updates -> To allow the user to make partial updates, use payload.dict(exclude_unset=True).items(). This ensures that only the specified fields get updated.
         if attr == "location_id":
             location = get_object_or_404(Location, id=value)
             device_model.location = location
@@ -49,6 +51,13 @@ def update_device(request, slug: str, device: DeviceUpdateSchema):
             setattr(device_model, attr, value)
     device_model.save()
     return device_model
+
+
+@app.delete("/devices/{slug}")
+def delete_device(request, slug: str):
+    device = get_object_or_404(Device, slug=slug)
+    device.delete()
+    return {"deleted": True}
 
 
 @app.post("/devices/{device_slug}/set-location", response=DeviceSchema)
--- devices/schemas.py
+++ devices/schemas.py
@@ -31,3 +31,11 @@ class DeviceLocationPatch(Schema):
 
 class DeviceUpdateSchema(DeviceCreateSchema):
     name: str | None = None
+
+    class Config:
+        """
+        Enforcing strict field validation
+        By default, any provided fields that don't exist in the schema will be silently ignored. To raise an error for these invalid fields, you can set extra = "forbid" in the schema's Config class. For example:
+        """
+
+        extra = "forbid"
