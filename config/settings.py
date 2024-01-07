a4f0147c59c46b20b284e84b6fd991fb18ac4c57
bekzod818 <bekzod030900@gmail.com>
2024-01-07 21:59:26 +0500

    fix: add_cors_header

--- config/settings.py
+++ config/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     # External_apps
     "django_extensions",
+    "corsheaders",
     # My_apps
     "devices.apps.DevicesConfig",
 ]
@@ -46,11 +47,19 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
+    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+]
+
+CORS_ALLOWED_ORIGINS = [
+    # "https://example.com",
+    # "https://sub.example.com",
+    "http://localhost:3000",
+    # "http://127.0.0.1:8080",
 ]
 
 ROOT_URLCONF = "config.urls"
