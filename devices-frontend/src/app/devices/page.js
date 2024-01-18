be588293a4bdfa33849e2f0e411a1920cec685b5
bekzod818 <bekzod030900@gmail.com>
2024-01-19 00:09:40 +0500

    fix: add_page_function

---
+++ devices-frontend/src/app/devices/page.js
@@ -0,0 +1,7 @@
+export default async function Devices() {
+    return (
+        <div className="flex flex-col items-center mt-2">
+            <h1 className="text-4xl">My Devices</h1>
+        </div>
+    )
+}
