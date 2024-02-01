c7426381786daa97f5155d044c62b9e61f90e997
bekzod818 <bekzod030900@gmail.com>
2024-02-01 08:05:36 +0500

    fix: updated

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -161,7 +161,7 @@ NINJA_JWT = {
     'UPDATE_LAST_LOGIN': False,
 
     'ALGORITHM': 'HS256',
-    'SIGNING_KEY': settings.SECRET_KEY,
+    'SIGNING_KEY': SECRET_KEY,
     'VERIFYING_KEY': None,
     'AUDIENCE': None,
     'ISSUER': None,
